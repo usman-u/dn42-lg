a18a14b704a6e8fe75932d15a17864af11d1bdd1
Usman <unumer@hotmail.com>
2023-03-01 16:19:43 +0000

    inventory: update IPs to hostnames

--- inventory.py
+++ inventory.py
@@ -6,20 +6,20 @@ routers = {
     # router name:    API/SSH  instance.
     "fr_lil1": net_automation.Vyos(
         device_type="vyos",
-        host="172.22.132.167",
+        host="fr-lil1.usman.dn42",
         username="test",
         password=os.getenv("fr_lil1_password"),
         use_keys=False,
-        url="https://172.22.132.167/graphql",
+        url="https://fr-lil1.usman.dn42/graphql",
         key=os.getenv("apikey"),
     ),
     "us_ca1": net_automation.Vyos(
         device_type="vyos",
-        host="172.22.132.164",
+        host="us-ca1.usman.dn42",
         username="lg",
         password=os.getenv("us_ca1_password"),
         use_keys=False,
-        url="https://172.22.132.164/graphql",
+        url="https://us-ca1.usman.dn42/graphql",
         key=os.getenv("apikey"),
     ),
 }
